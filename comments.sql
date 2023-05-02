bedc9a19fc346356175e28378032a8c1a114ef69
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2023-05-02 18:14:35 +0300

    Add files via upload

---
+++ comments.sql
@@ -0,0 +1,17 @@
+prompt --application/comments
+begin
+--   Manifest
+--     APPLICATION COMMENTS: 166271
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2023.04.28'
+,p_release=>'23.1.0-16'
+,p_default_workspace_id=>11151152342368489839
+,p_default_application_id=>166271
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_CARGOMANAGEMENTSYSTEM'
+);
+null;
+wwv_flow_imp.component_end;
+end;
+/
